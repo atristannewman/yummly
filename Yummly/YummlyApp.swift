19aeee647da43938b3dd775a29b697f51a32bed4
Tristan <tbnewman2@gmail.com>
2024-11-11 11:47:43 +0200

    initial commit

---
+++ Yummly/YummlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  YummlyApp.swift
+//  Yummly
+//
+//  Created by Tristan Newman on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct YummlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
